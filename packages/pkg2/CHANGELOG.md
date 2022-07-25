24667eb0b0a42d9979f87ef54fd1eaacc734947e
JeromeLin <jerome.lin@zhongan.com>
2022-07-25 10:53:14 +0800

    perf: optimize pkg2

--- packages/pkg2/CHANGELOG.md
+++ packages/pkg2/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @keeljs/pkg2
 
+## 1.0.2
+
+### Patch Changes
+
+- perf: pkg2 666
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/pkg2/index.js
+++ packages/pkg2/index.js
@@ -2,7 +2,7 @@ import a from 'pkg1';
 
 const b = () => {
   a();
-  console.log('this is pkg2');
+  console.log('this is pkg2, hahaha123');
 }
 
 export default b;
