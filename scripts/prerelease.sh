2e675e044ebf6b414c3fdfd92b55919bd6a48462
JeromeLin <jerome.lin@zhongan.com>
2022-07-26 18:41:33 +0800

    release: new beta packages

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -2,9 +2,10 @@
 
 changeset pre enter beta
 changeset version
-changeset pre exit
 pnpm install
 git add .
 git commit -m "release: new beta packages"
 changeset publish
 git push origin main --follow-tags
+changeset pre exit
+
