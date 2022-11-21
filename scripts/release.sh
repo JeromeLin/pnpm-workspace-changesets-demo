a59f38f5ba99cceac6c01047dd2cde572b0f2640
JeromeLin <jerome.lin@zhongan.com>
2022-11-21 15:46:38 +0800

    chore: update

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,12 +4,11 @@ changeset
 changeset version
 
 pnpm install
-
-git add .
-git commit -m "release: new version packages"
-
 pnpm lint
 pnpm build
 
 changeset publish
+
+git add .
+git commit -m "release: new version packages"
 git push origin main --follow-tags
